5f95855f39469b239d34ba3f5fed745472456360
Reuben Lifshay <rlifshay@gmail.com>
2026-07-26 18:39:29 -0700

    wip: add StaticTemplateResolver

---
+++ tests/PHPTemplate/StaticTemplateResolverTest.php
@@ -0,0 +1,65 @@
+<?php declare(strict_types=1);
+
+use Computator\FrameworkUtils\PHPTemplate\StaticTemplateResolver;
+use Computator\FrameworkUtils\PHPTemplate\TemplateBase;
+use Computator\FrameworkUtils\PHPTemplate\TextTemplate;
+use PHPUnit\Framework\TestCase;
+
+final class StaticTemplateResolverTest extends TestCase {
+	public function testTemplateMappingFound(): void {
+		$tc_success = new class ('') extends TemplateBase {
+			public function __construct(
+				public readonly string $value,
+			) {}
+			public function execute(mixed ...$__context): mixed {
+				return null;
+			}
+			public function get_contents(int $offset = 0, int|null $length = null): string {
+				return "";
+			}
+		};
+
+		$resolved = (new StaticTemplateResolver(
+			[
+				'asdf' => 'one',
+				'qwer' => 'two',
+			],
+			$tc_success::class,
+		))->resolve('asdf');
+		$this->assertNotNull($resolved);
+		$this->assertEquals('one', $resolved->value);
+	}
+
+	public function testTemplateMappingNotFound(): void {
+		$tc_success = new class ('') extends TemplateBase {
+			public function __construct(
+				public readonly string $value,
+			) {}
+			public function execute(mixed ...$__context): mixed {
+				return null;
+			}
+			public function get_contents(int $offset = 0, int|null $length = null): string {
+				return "";
+			}
+		};
+
+		$resolved = (new StaticTemplateResolver(
+			[
+				'asdf' => 'one',
+				'qwer' => 'two',
+			],
+			$tc_success::class,
+		))->resolve('invalid');
+		$this->assertNull($resolved);
+	}
+
+	public function testDefaultTemplateClass(): void {
+		$resolved = (new StaticTemplateResolver([
+			'asdf' => 'one',
+			'qwer' => 'two',
+		]))->resolve('asdf');
+		$this->assertNotNull($resolved);
+		$this->assertInstanceOf(TextTemplate::class, $resolved);
+		$this->assertEquals('one', $resolved->content);
+	}
+}
