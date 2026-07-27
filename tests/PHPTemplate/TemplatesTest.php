f4f9e80a9b78330c6783cf4e192d59ff4645fb49
Reuben Lifshay <rlifshay@gmail.com>
2026-07-26 21:19:58 -0700

    feat: add TemplateRenderProxy and make child templates work

---
+++ tests/PHPTemplate/TemplatesTest.php
@@ -0,0 +1,64 @@
+<?php declare(strict_types=1);
+
+use Computator\FrameworkUtils\PHPTemplate\Renderer;
+use Computator\FrameworkUtils\PHPTemplate\StaticTemplateResolver;
+use Computator\FrameworkUtils\PHPTemplate\TemplateResolver;
+use Computator\FrameworkUtils\PHPTemplate\TextTemplate;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+
+final class TemplatesTest extends TestCase {
+	#[DataProvider('templatesProvider')]
+	public function testTemplate(string $template, array $deps, string $expected): void {
+		$r = new Renderer(
+			new TextTemplate($template),
+			new StaticTemplateResolver($deps),
+		);
+		$out = $r->renderToString();
+		$this->assertEquals($expected, $out);
+	}
+
+	public static function templatesProvider(): array {
+		return [
+			'empty' => [
+				<<<'INPUT'
+				INPUT,
+				[],
+				'',
+			],
+			'basic text' => [
+				<<<'INPUT'
+				asdf
+				INPUT,
+				[],
+				'asdf',
+			],
+			'basic code' => [
+				<<<'INPUT'
+				<?php
+				echo "asdf";
+				INPUT,
+				[],
+				'asdf',
+			],
+			'child template' => [
+				<<<'INPUT'
+				parent before
+				<? self::tpl('child_tpl')() ?>
+				parent after
+				INPUT,
+				[
+					'child_tpl' => <<<'CHILD'
+					child
+					:
+					CHILD,
+				],
+				<<<'EXPECTED'
+				parent before
+				child
+				:parent after
+				EXPECTED,
+			],
+		];
+	}
+}
