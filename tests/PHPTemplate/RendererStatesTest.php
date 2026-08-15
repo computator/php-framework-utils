e6a98f4aab8ac8fc36ccf69bf2a4e15f324faaa0
Reuben Lifshay <rlifshay@gmail.com>
2026-08-15 02:56:50 -0700

    feat: update Renderer to use nested nodes in rendertree

--- tests/PHPTemplate/RendererStatesTest.php
+++ tests/PHPTemplate/RendererStatesTest.php
@@ -4,6 +4,7 @@ namespace Computator\FrameworkUtils\Test\PHPTemplate;
 
 use Computator\FrameworkUtils\PHPTemplate\RenderManager;
 use Computator\FrameworkUtils\PHPTemplate\Renderer;
+use Computator\FrameworkUtils\PHPTemplate\RenderTree\Tree;
 use Computator\FrameworkUtils\PHPTemplate\Templates;
 use Computator\FrameworkUtils\Test\PHPTemplate\TestUtils;
 use PHPUnit\Framework\Attributes\CoversClass;
@@ -38,7 +39,59 @@ final class RendererStatesTest extends TestCase {
 		$r->renderChild($r->getTemplateAsProxy('test_tpl'));
 		$r->renderChild($r->getTemplateAsProxy('test_tpl'));
 
+		$this->assertJsonStringEqualsJsonString(
+			json_encode(
+				[
+					"asdf",
+					"qwer",
+					"zxcv",
+				]
+			),
+			json_encode(Tree::map_structure($r->rendertree->root)),
+		);
+
 		$this->expectOutputString('asdfqwerzxcv');
+		$r->rendertree->render();
+	}
+
+	public function testNestedChild(): void {
+		/** @var RenderManager|TestUtils\VisibleRenderer $r */
+		$r = TestUtils\VisibleRenderer::create(
+			$this->stubTemplate('root'),
+			new TestUtils\QueueTemplateResolver(
+				$this->stubTemplate('asdf'),
+				$this->stubTemplate(function (...$n) {
+					echo "A";
+					$n['renderer']->renderChild($n['renderer']->getTemplateAsProxy('test_tpl'));
+					echo "B";
+				}),
+				$this->stubTemplate('qwer'),
+				$this->stubTemplate('zxcv'),
+			),
+		);
+
+		$r->renderChild($r->getTemplateAsProxy('test_tpl')); // asdf
+		$r->renderChild($r->getTemplateAsProxy('test_tpl')); // A + qwer + B
+		$r->renderChild($r->getTemplateAsProxy('test_tpl')); // zxcv
+
+		$this->assertJsonStringEqualsJsonString(
+			json_encode(
+				[
+					"asdf",
+					[
+						"A",
+						[
+							"qwer",
+						],
+						"B",
+					],
+					"zxcv",
+				]
+			),
+			json_encode(Tree::map_structure($r->rendertree->root)),
+		);
+
+		$this->expectOutputString('asdfAqwerBzxcv');
 		$r->rendertree->render();
 	}
 }
